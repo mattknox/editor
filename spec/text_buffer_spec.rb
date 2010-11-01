1b5eb6e3ff66a631f3749ca92af8b0c6787e85f9
matt knox <matthewknox@gmail.com>
2010-11-01 11:31:55 -0700

    fixed specs

--- spec/text_buffer_spec.rb
+++ spec/text_buffer_spec.rb
@@ -19,12 +19,12 @@ this
 FOO
 
 describe TextBuffer do
-  describe "text" do
+  describe "to_s" do
     it "should reply to text with the text put in" do
       t = TextBuffer.new
       t.text = $str
 
-      t.text.should == $str
+      t.to_s.should == $str
     end
   end
 end
